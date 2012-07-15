34e2871eca0a4aae329772da34cf977a7e5d22d7
naoshi <naoshi@10.0.0.7>
2012-07-15 17:54:54 +0900

    Reformat.

--- tuple/src/main/java/jp/nhiguchi/libs/tuple/Pair.java
+++ tuple/src/main/java/jp/nhiguchi/libs/tuple/Pair.java
@@ -6,54 +6,46 @@ import java.util.Objects;
  *
  * @author Naoshi Higuchi
  */
-public final class Pair<E1, E2>
-{
+public final class Pair<E1, E2> {
 	private final E1 fElem1;
 	private final E2 fElem2;
 
-	private Pair(E1 elem1, E2 elem2)
-	{
+	private Pair(E1 elem1, E2 elem2) {
 		fElem1 = elem1;
 		fElem2 = elem2;
 	}
 
-	public static <E1, E2> Pair newPair(E1 elem1, E2 elem2)
-	{
+	public static <E1, E2> Pair newPair(E1 elem1, E2 elem2) {
 		return new Pair(elem1, elem2);
 	}
 
-	public E1 get1st()
-	{
+	public E1 get1st() {
 		return fElem1;
 	}
 
-	public E2 get2nd()
-	{
+	public E2 get2nd() {
 		return fElem2;
 	}
 
 	@Override
-	public boolean equals(Object obj)
-	{
+	public boolean equals(Object obj) {
 		if (obj == null) return false;
 		if (obj == this) return true;
 		if (!(obj instanceof Pair)) return false;
 
-		Pair rhs = (Pair)obj;
-		
-		return Objects.equals(fElem1, rhs.fElem1) &&
-				Objects.equals(fElem2, rhs.fElem2);
+		Pair rhs = (Pair) obj;
+
+		return Objects.equals(fElem1, rhs.fElem1)
+				&& Objects.equals(fElem2, rhs.fElem2);
 	}
 
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		return Objects.hashCode(fElem1) + Objects.hashCode(fElem2);
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return String.format("<%s, %s>", fElem1, fElem2);
 	}
 }
--- tuple/src/main/java/jp/nhiguchi/libs/tuple/Triple.java
+++ tuple/src/main/java/jp/nhiguchi/libs/tuple/Triple.java
@@ -6,47 +6,40 @@ import java.util.Objects;
  *
  * @author Naoshi Higuchi
  */
-public final class Triple<E1, E2, E3>
-{
+public final class Triple<E1, E2, E3> {
 	private final E1 fElem1;
 	private final E2 fElem2;
 	private final E3 fElem3;
 
-	private Triple(E1 elem1, E2 elem2, E3 elem3)
-	{
+	private Triple(E1 elem1, E2 elem2, E3 elem3) {
 		fElem1 = elem1;
 		fElem2 = elem2;
 		fElem3 = elem3;
 	}
 
-	public static <E1, E2, E3> Triple newTriple(E1 elem1, E2 elem2, E3 elem3)
-	{
+	public static <E1, E2, E3> Triple newTriple(E1 elem1, E2 elem2, E3 elem3) {
 		return new Triple(elem1, elem2, elem3);
 	}
 
-	public E1 get1st()
-	{
+	public E1 get1st() {
 		return fElem1;
 	}
 
-	public E2 get2nd()
-	{
+	public E2 get2nd() {
 		return fElem2;
 	}
 
-	public E3 get3rd()
-	{
+	public E3 get3rd() {
 		return fElem3;
 	}
 
 	@Override
-	public boolean equals(Object obj)
-	{
+	public boolean equals(Object obj) {
 		if (obj == null) return false;
 		if (obj == this) return true;
 		if (!(obj instanceof Triple)) return false;
 
-		Triple rhs = (Triple)obj;
+		Triple rhs = (Triple) obj;
 
 		return Objects.equals(fElem1, fElem2)
 				&& Objects.equals(fElem2, rhs.fElem2)
@@ -54,16 +47,14 @@ public final class Triple<E1, E2, E3>
 	}
 
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		return Objects.hashCode(fElem1)
 				+ Objects.hashCode(fElem2)
 				+ Objects.hashCode(fElem3);
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return String.format("<%s, %s, %s>", fElem1, fElem2, fElem3);
 	}
 }
